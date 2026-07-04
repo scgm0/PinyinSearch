235ae7c70412e4251b7bae7346e09a254302227b
scgm0 <2682963017@qq.com>
2026-07-04 23:36:12 +0800

    refactor: 更新BetterHandbook兼容补丁，适配最新版本

--- PinyinSearch/BetterHandbookCompat.cs
+++ PinyinSearch/BetterHandbookCompat.cs
@@ -1,13 +1,10 @@
-using System.Collections.Generic;
-using System.Reflection.Emit;
-using System.Text.RegularExpressions;
 using HarmonyLib;
 
 namespace PinyinSearch;
 
 [HarmonyPatchCategory("betterhandbook")]
 public static class BetterHandbookCompat {
-	static private string _searchText = string.Empty;
+	private static string _searchText = string.Empty;
 
 	[HarmonyPatch("HandbookCache.HandbookFilterCachePatch", "BuildResults")]
 	[HarmonyPrefix]
@@ -15,24 +12,9 @@ public static class BetterHandbookCompat {
 		_searchText = searchText;
 	}
 
-	[HarmonyPatch("HandbookCache.HandbookFilterCachePatch", "BuildResults")]
-	[HarmonyTranspiler]
-	public static IEnumerable<CodeInstruction> BuildResultsTranspiler(IEnumerable<CodeInstruction> instructions) {
-		var originalMethod = AccessTools.Method("HandbookCache.HandbookFilterCachePatch:CountMatches");
-		var customMethod = AccessTools.Method(typeof(BetterHandbookCompat), nameof(CustomCountMatches));
-
-		foreach (var instruction in instructions) {
-			if (instruction.Calls(originalMethod)) {
-				yield return new(OpCodes.Call, customMethod);
-			} else {
-				yield return instruction;
-			}
-		}
-	}
-
-	public static int CustomCountMatches(string text, Regex regex) {
-		var originalCount = regex.Count(text);
-		var pinyinCount = PinyinSearchModSystem.Matcher?.CountMatches(text, _searchText) * 100 ?? 0;
-		return originalCount + pinyinCount;
+	[HarmonyPatch("HandbookCache.HandbookFilterCachePatch", "CountMatches")]
+	[HarmonyPostfix]
+	public static void CountMatchesPostfix(ref int __result, string text) {
+		__result += PinyinSearchModSystem.Matcher?.CountMatches(text, _searchText) * 100 ?? 0;
 	}
 }
